4cdac9b58d093f788271e9d8a8a90954264f0b00
Chase Johnson <Chase.b.johnson@gmail.com>
2020-06-01 18:07:54 -0700

    Added user questions under index.js.

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
 // Required packages for generator 
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
 
+// inquirer will start to gather information to write README.md file
+function promptUser () {
+    return inquirer.prompt([
+        {
+            type: `input`,
+            name: `repo`,
+            message: `Create a new repository.`,
+            validate: validator
+        },
+        {
+            type: `input`,
+            name: `description`,
+            message: `Write a short description for your project.`, 
+            validate: validator
+        },
+        {
+            type: `input`,
+            name: `author`,
+            message: `Enter the authors first and last name.`, 
+            validate: validator
+        },
+        {
+            type: `input`,
+            name: `github`,
+            message: `Enter your GitHub Username.`, 
+            validate: validator
+        },
+        {
+            type: `input`,
+            name: `install`,
+            message: `How's your application installed?`, 
+            validate: validator
+        },
+        {
+            type: `input`,
+            name: `license`,
+            message: `Would you like to add a license for your project? Press enter to skip.`, 
+            validate: validator
+        },
+        {
+            type: `input`,
+            name: `contributors`,
+            message: `List any additional contributors. Press enter if you're the only one.`, 
+            validate: validator
+        },
+        {
+            type: `input`,
+            name: `FAQ`,
+            message: `For further questions please enter your email.`, 
+            validate: validator
+        },
+    ]);
+}
